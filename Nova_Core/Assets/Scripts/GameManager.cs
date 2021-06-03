47c5bc4ac5617370e13867e2567ac5403905f150
Kazma <mariocastro1205@gmail.com>
2021-06-02 23:18:24 -0700

    Character controller
    Created Character Controller, and test environment

--- Nova_Core/Assets/Scripts/GameManager.cs
+++ Nova_Core/Assets/Scripts/GameManager.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public static GameManager Instance;
+    public int coins;
+    public int hp;
+    public int potions;
+
+    public int keysNeeded = 3;
+    public bool[] keys;
+
+    public bool a1;
+    public bool a2;
+    public bool a3;
+    public bool a4;
+
+    public int b1 = 0;
+    public int b2;
+    public int b3;
+    public int b4;
+
+    public bool lava = false;
+
     void Start()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /*
+
+    private void Update()
     {
-        
-    }
+        if (b1 >= 300) {
+            b1 = 300;
+        }
+        if (b2 >= 200)
+        {
+            b2 = 200;
+        }
+        if (b3 >= 200)
+        {
+            b3 = 200;
+        }
+        if (b4 >= 100)
+        {
+            b4 = 100;
+        }
+    }*/
 }
