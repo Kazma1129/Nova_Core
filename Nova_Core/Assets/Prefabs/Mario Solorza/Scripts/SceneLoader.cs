b93e83c27ddf0dd64180507d9ebe5a075bd143ce
Kazma <mariocastro1205@gmail.com>
2021-06-17 12:47:40 -0700

    Added Menu
    Added Menu Logic, Exit, and credit sections...created the first build of the game

---
+++ Nova_Core/Assets/Prefabs/Mario Solorza/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void sceneStart() {
+        SceneManager.LoadScene("Test Enviroment Mario Solorza");
+    }
+
+    public void mainScreen() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void salir() {
+        Application.Quit();
+    }
+
+    public void credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+ 
+
+}
