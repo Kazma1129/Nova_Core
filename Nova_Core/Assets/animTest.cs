887043b593f7c2b845149baf96371e787850b7f0
Kazma <mariocastro1205@gmail.com>
2021-07-31 00:43:36 -0700

    added character for testing animation and rig
    added character for testing animation and rig

---
+++ Nova_Core/Assets/animTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animTest : MonoBehaviour
+{
+
+    private Animation anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.Play("Idle");
+    }
+}
