983054032380381334057e158a2f1601f8ba6ef4
Kazma <mariocastro1205@gmail.com>
2021-08-06 13:34:53 -0700

    test rigs
    tested rigs

---
+++ Nova_Core/Assets/Prefabs/Mario Solorza/Scripts/animationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationController : MonoBehaviour
+{
+
+    Animation anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.Play("Idle");
+    }
+}
